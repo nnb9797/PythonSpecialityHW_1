3c6a12b22b01ab972c036a9b000a33171ea52eec
nnb9797 <bushueva.n.n@mail.ru>
2023-07-29 05:40:04 +0700

    seventh commit

--- HW_7/new_dir_creating.py
+++ HW_7/new_dir_creating.py
@@ -1,12 +1,11 @@
-from create_files_2 import create_files_2
 from pathlib import Path
 import os
 
 
 def new_dir_creating(name_dir: str):
     name = Path(Path.cwd() / name_dir)
-    if not name.exists():       # проверка на наличие директория
-        name.mkdir()            # создает директорий с именем name_dir в текущем директории
-    os.chdir(name)          # переходим в созданный каталог сделав его текущим
+    if not name.exists():       # проверка наличия дир-ии
+        name.mkdir()            # создание директории
+    os.chdir(name)              # переход в созданный каталог, делая его текущим
 
 
